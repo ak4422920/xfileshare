f510af2628bd76f86ea4537f54f9a9dd5d1be820
ak4422920 <ak4422920@gmail.com>
2025-01-13 21:28:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -29,7 +29,7 @@ TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 try:
     ADMINS=[6848088376]
-    for x in (os.environ.get("ADMINS", "1049173662").split()):
+    for x in (os.environ.get("ADMINS", "1049173662 7781222730").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
